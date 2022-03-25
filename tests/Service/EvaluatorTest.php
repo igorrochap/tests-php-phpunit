f9f490cf7dbf77b1221f68f890d59809b1dba231
igorrochap <igor.systemof@gmail.com>
2022-03-25 16:52:02 -0300

    Organizing the tests

--- tests/Service/EvaluatorTest.php
+++ tests/Service/EvaluatorTest.php
@@ -10,17 +10,13 @@ use PHPUnit\Framework\TestCase;
 
 class EvaluatorTest extends TestCase
 {
-    public function testEvaluatorShouldFindTheHighestValueInAscendingOrder()
+    /**
+     * @dataProvider ascendingOrderAuction
+     * @dataProvider descendingOrderAuction
+     * @dataProvider randomOrderAuction
+     */
+    public function testEvaluatorShouldFindTheHighestValueOfBids(Auction $auction)
     {
-        //arrange - given
-        $auction = new Auction('Fiat 147 0km');
-
-        $maria = new User('Maria');
-        $joao = new User('João');
-
-        $auction->receiveBid(new Bid($joao, 2000));
-        $auction->receiveBid(new Bid($maria, 2500));
-
         $auctioneer = new Evaluator();
 
         //act - when
@@ -31,81 +27,92 @@ class EvaluatorTest extends TestCase
         self::assertEquals(2500, $highestValue);
     }
 
-    public function testEvaluatorShouldFindTheHighestValueInDescendingOrder()
+    /**
+     * @dataProvider ascendingOrderAuction
+     * @dataProvider descendingOrderAuction
+     * @dataProvider randomOrderAuction
+     */
+    public function testEvaluatorShouldFindTheLowestValueOfBids(Auction $auction)
     {
-        $auction = new Auction('Fiat 147 0km');
-
-        $maria = new User('Maria');
-        $joao = new User('João');
-
-        $auction->receiveBid(new Bid($maria, 2500));
-        $auction->receiveBid(new Bid($joao, 2000));
-
-        $auctioneer = new Evaluator();
-
-        $auctioneer->evaluate($auction);
-        $highestValue = $auctioneer->getHighestValue();
-
-        self::assertEquals(2500, $highestValue);
-    }
-
-    public function testEvaluatorShouldFindTheLowestValueInDescendingOrder()
-    {
-        $auction = new Auction('Fiat 147 0km');
-
-        $maria = new User('Maria');
-        $joao = new User('João');
-
-        $auction->receiveBid(new Bid($maria, 2500));
-        $auction->receiveBid(new Bid($joao, 2000));
-
         $auctioneer = new Evaluator();
 
         $auctioneer->evaluate($auction);
         $lowestValue = $auctioneer->getLowestValue();
 
-        self::assertEquals(2000, $lowestValue);
+        self::assertEquals(1300, $lowestValue);
     }
 
-    public function testEvaluatorShouldFindTheLowestValueInAscendingOrder()
+    /**
+     * @dataProvider ascendingOrderAuction
+     * @dataProvider descendingOrderAuction
+     * @dataProvider randomOrderAuction
+     */
+    public function testEvaluatorShouldFindTheThreeHighestValues(Auction $auction)
     {
-        $auction = new Auction('Fiat 147 0km');
-
-        $maria = new User('Maria');
-        $joao = new User('João');
-
-        $auction->receiveBid(new Bid($joao, 2000));
-        $auction->receiveBid(new Bid($maria, 2500));
-
-        $auctioneer = new Evaluator();
-
-        $auctioneer->evaluate($auction);
-        $lowestValue = $auctioneer->getLowestValue();
-
-        self::assertEquals(2000, $lowestValue);
-    }
-
-    public function testEvaluatorShouldFindTheThreeHighestValues()
-    {
-        $auction = new Auction('Fiat 147 0Km');
-
-        $joao = new User('João');
-        $maria = new User('Maria');
-        $rogerio = new User('Rogerio');
-        $vanessa = new User('Vanessa');
-
-        $auction->receiveBid(new Bid($rogerio, 1500));
-        $auction->receiveBid(new Bid($maria, 1300));
-        $auction->receiveBid(new Bid($vanessa, 2000));
-        $auction->receiveBid(new Bid($joao, 1900));
-
         $auctioneer = new Evaluator();
         $auctioneer->evaluate($auction);
         $highestBids = $auctioneer->getHighestBids();
 
         self::assertCount(3, $highestBids);
-        self::assertEquals(2000, $highestBids[0]->getValue());
-        self::assertEquals(1900, $highestBids[1]->getValue());
-        self::assertEquals(1500, $highestBids[2]->getValue());
+        self::assertEquals(2500, $highestBids[0]->getValue());
+        self::assertEquals(2000, $highestBids[1]->getValue());
+        self::assertEquals(1700, $highestBids[2]->getValue());
+    }
+
+    public function ascendingOrderAuction(): array
+    {
+        $auction = new Auction('Fiat 147 0km');
+
+        $maria = new User('Maria');
+        $joao = new User('João');
+        $vanessa = new User('Vanessa');
+        $rogerio = new User('Rogerio');
+
+        $auction->receiveBid(new Bid($rogerio, 1300));
+        $auction->receiveBid(new Bid($vanessa, 1700));
+        $auction->receiveBid(new Bid($joao, 2000));
+        $auction->receiveBid(new Bid($maria, 2500));
+
+        return [
+            [$auction]
+        ];
+    }
+
+    public function descendingOrderAuction(): array
+    {
+        $auction = new Auction('Fiat 147 0km');
+
+        $maria = new User('Maria');
+        $joao = new User('João');
+        $vanessa = new User('Vanessa');
+        $rogerio = new User('Rogerio');
+
+        $auction->receiveBid(new Bid($maria, 2500));
+        $auction->receiveBid(new Bid($joao, 2000));
+        $auction->receiveBid(new Bid($vanessa, 1700));
+        $auction->receiveBid(new Bid($rogerio, 1300));
+
+        return [
+            [$auction]
+        ];
+    }
+
+    public function randomOrderAuction(): array
+    {
+        $auction = new Auction('Fiat 147 0km');
+
+        $maria = new User('Maria');
+        $joao = new User('João');
+        $vanessa = new User('Vanessa');
+        $rogerio = new User('Rogerio');
+
+        $auction->receiveBid(new Bid($maria, 2500));
+        $auction->receiveBid(new Bid($vanessa, 1700));
+        $auction->receiveBid(new Bid($joao, 2000));
+        $auction->receiveBid(new Bid($rogerio, 1300));
+
+        return [
+            [$auction]
+        ];
     }
 }
